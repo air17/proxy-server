9c1ea05dd3a57e7b8317a988ffe2ecc8f5349cc2
air17 <ou1705@gmail.com>
2022-08-09 00:07:04 +0300

    Add app files

---
+++ main.py
@@ -0,0 +1,62 @@
+from aiohttp import web
+from bs4 import BeautifulSoup
+import httpx
+import re
+
+original_domain = "https://news.ycombinator.com"
+proxy_domain = "http://127.0.0.1:8000"
+
+
+def add_trademark(document: str) -> str:
+    """Adds ™ sign to every six letters word in the text of the HTML document"""
+
+    six_letters_word = re.compile(r"\b\w{6}\b")
+
+    soup = BeautifulSoup(document, features="lxml")
+    word_elements = soup.find_all(text=six_letters_word)
+    for el in word_elements:
+        end_of_word = six_letters_word.search(str(el)).end()
+        new_text = el[:end_of_word] + "™" + el[end_of_word:]
+        el.replace_with(new_text)
+
+    return str(soup)
+
+
+async def proxify(request):
+    # getting original page or file
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            original_domain + str(request.rel_url),
+            headers=dict(request.headers),
+        )
+
+    content_type_header = response.headers.get("content-type")
+    expires_header = response.headers.get("expires")
+
+    # removing charset info, leaving only content type
+    content_type = content_type_header.split(";")[0] if content_type_header else None
+
+    if "text" not in content_type:
+        return web.Response(
+            body=response.content,
+            content_type=content_type,
+            headers={"Expires": expires_header},
+        )
+
+    document = response.text.replace(original_domain, proxy_domain)
+
+    if "html" in content_type:
+        document = add_trademark(document)
+
+    return web.Response(text=document, content_type=content_type)
+
+
+def main():
+    app = web.Application()
+    app.router.add_get("/{path:.*}", proxify)
+
+    web.run_app(app, host="127.0.0.1", port=8000, access_log=None)
+
+
+if __name__ == "__main__":
+    main()
